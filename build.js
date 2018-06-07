5eca95e5ed377ca694a1ac056ad774e9303b8461
auruscha <auruscha@protonmail.com>
2018-06-07 10:24:06 +0000

    update

--- build.js
+++ build.js
@@ -2,27 +2,30 @@ var Metalsmith  = require('metalsmith');
 var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var permalinks  = require('metalsmith-permalinks');
-const assets      = require('mrs-smith-assets');
+const assets    = require('metalsmith-assets');
+const inlinesource = require('metalsmith-inline-source');
 
 Metalsmith(__dirname)
 .metadata({
-  title: "auRuscha",
-  description: "auRuscha's blog",
-  generator: "Metalsmith",
-  url: "https://auruscha.github.io/"
+  title: "CrazyPinkWidow",
+  description: "CrazyPinkWidow's Blog",
+  url: "https://crazypinkwidow.github.io/"
 })
 .source('src')
 .destination('public')
-.clean(false)
-  .use(assets({
-    source: 'assets',
-    destination: '.'
-  }))
+.clean(true)
+.use(assets({
+  source: 'assets',
+  destination: '.'
+}))
 .use(markdown())
 .use(permalinks())
 .use(layouts({
   directory: 'layouts',
   engine: 'handlebars'
+}))
+.use(inlinesource({
+  compress: false
 }))
 .build(function(err, files) {
   if (err) { throw err; }
